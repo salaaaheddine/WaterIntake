13652c3e158c2729c6a76542130ec0115e05afa6
Salah <theonegreat0@gmail.com>
2024-02-04 16:38:32 +0100

    first commit

---
+++ src/main/java/com/waterIntake/WaterIntakeApplication.java
@@ -0,0 +1,13 @@
+package com.waterIntake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterIntakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterIntakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waterIntake/WaterIntakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waterIntake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterIntakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
